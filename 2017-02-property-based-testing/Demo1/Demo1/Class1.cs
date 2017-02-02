4fc1244504f5ed7a9c3fb1be48c1af3bf9224e8e
Patrick Drechsler <socialcoding@pdrechsler.de>
2017-02-02 22:51:03 +0100

    initial commit

---
+++ 2017-02-property-based-testing/Demo1/Demo1/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FsCheck;
+using Xunit;
+
+namespace Demo1
+{
+    public class Class1
+    {
+        [Fact]
+        public void SomeTest()
+        {
+            Func<int[], bool> revRevIsOrig = xs => xs.Reverse().SequenceEqual(xs);
+            Prop.ForAll(revRevIsOrig).QuickCheckThrowOnFailure();
+        }
+
+
+        public int Add3(int i)
+        {
+            return i+3;
+            if (i == 0) return 3;
+            if (i == 1) return 4;
+            return 99;
+        }
+
+        [Fact]
+        public void SomeOtherTest()
+        {
+            Func<int, bool> f = n => Add3(n) == n + 3;
+            Prop.ForAll(f).QuickCheckThrowOnFailure();
+
+        }
+    }
+}
---
+++ 2017-02-property-based-testing/Demo1/Demo1/Kata.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using FsCheck;
+using Xunit;
+
+namespace Demo1
+{
+    public class Diamond
+    {
+        private static string Padding(int i)
+        {
+            return new String(' ', i);
+        }
+
+        
+
+        public static string CreateDiamond(uint diamondSize)
+        {
+            if (diamondSize == 1) return "A";
+
+            var upperHalfOfDiamond = new List<string>();
+
+            for (int currentRow = 0; currentRow < diamondSize-1; currentRow++)
+            {
+                if (currentRow == 0)
+                {
+                    var result = Padding((int) (diamondSize - 1));
+                    result += "A";
+                    result += Padding((int) (diamondSize - 1)) + '\n';
+                    upperHalfOfDiamond.Add(result);
+                }
+                else
+                {
+                    var formattedRow = FormattedRow(diamondSize, currentRow);
+                    upperHalfOfDiamond.Add(formattedRow);
+                }
+
+            }
+
+            var middleLine = FormattedRow(diamondSize, (int) diamondSize - 1);
+
+            var lowerHalfofDiamond = new List<string>(upperHalfOfDiamond);
+            lowerHalfofDiamond.Reverse();
+
+            return string.Join("", upperHalfOfDiamond) + middleLine + string.Join("", lowerHalfofDiamond);
+        }
+
+        private static string FormattedRow(uint i, int currentRow)
+        {
+            var rowPadding = i - (currentRow + 1);
+            //rowPadding = rowPadding < 0 ? 0 : rowPadding;
+            var formattedRow = string.Empty;
+            formattedRow += Padding((int) (rowPadding));
+            formattedRow += (char) (65 + currentRow); // " B..."
+            formattedRow += Padding((int) (2 * currentRow - 1));
+            formattedRow += (char) (65 + currentRow); // " B B "
+            formattedRow += Padding((int) (rowPadding)) + '\n';
+            return formattedRow;
+        }
+    }
+
+    public class DiamondTests
+    {
+        [Fact]
+        public void ZeroSizeShouldReturnEmptyString()
+        {
+            var diamond = Diamond.CreateDiamond(0);
+            diamond.Should().Be("");
+        }
+
+        [Fact]
+        public void SizeOneShouldReturnStringA()
+        {
+            var diamond = Diamond.CreateDiamond(3);
+            diamond.Should().Be("A");
+        }
+
+        [Fact]
+        public void EachRowShouldOnlyContains_SingleCharacter_AndOr_Space()
+        {
+            Func<uint, bool> f = (n) =>
+            {
+                if (n <= 1) return true;
+                var rows = Diamond.CreateDiamond(n).Split(new []{'\n'}, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var row in rows)
+                {
+                    row.Distinct().Count().Should().Be(2);
+                    row.Contains(' ').Should().BeTrue();
+                }
+
+                return true;
+            };
+            Prop.ForAll(f).QuickCheckThrowOnFailure();
+        }
+
+        [Fact]
+        public void NumberOfRowsIsAlwaysOdd()
+        {
+            Func<uint, bool> f = (n) =>
+            {
+                if (n <= 1) return true;
+                var rows = Diamond.CreateDiamond(n).Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                return rows.Length % 2 == 1;
+            };
+            Prop.ForAll(f).QuickCheckThrowOnFailure();
+        }
+
+        [Fact]
+        public void NumberOfRowsIsAlways_2n_Minus_One()
+        {
+            Func<uint, bool> f = (n) =>
+            {
+                if (n <= 1) return true;
+                var rows = Diamond.CreateDiamond(n).Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                return rows.Length == 2 * n -1;
+            };
+            Prop.ForAll(f).QuickCheckThrowOnFailure();
+        }
+
+        [Fact]
+        public void Dummy()
+        {
+            var s = " A";
+            s.Distinct().Count().Should().Be(2);
+
+        }
+    }
+}
